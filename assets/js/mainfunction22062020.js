cb898ff011a77bced76d8cc98b6b1d10163587a7
gokilay <98450415+gokilay@users.noreply.github.com>
2022-06-24 10:47:38 +0200

    Update mainfunction22062020.js

--- assets/js/mainfunction22062020.js
+++ assets/js/mainfunction22062020.js
@@ -7,7 +7,7 @@ function serilestir2() {
     return $("#yuk").html('<img style="height:70px;width:70px" src="/loader.webp" /> '), $("#StoryButton").attr("disabled", !0), $("#StoryButton").attr("title", "Loading..."), $("#StoryButton").css("cursor", "progress"),
     $.ajax({
         type: "POST",
-        url: 'https://127.0.0.1:5000/api/instagram',
+        url: 'https://api.tigkram.ml/api/instagram',
         data:"#text_username",
         async:true,
         crossDomain:true,
